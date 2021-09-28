0f8d0a6dbd800dbcee85045fa069a8fb75336eda
Costa <costa.van.elsas@hva.nl>
2021-09-28 14:48:49 +0200

    Added dummy data to workout page

--- lib/feature/workout/ui/workout.dart
+++ lib/feature/workout/ui/workout.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Workout extends StatefulWidget {
-  const Workout({Key? key}) : super(key: key);
+import '../../../workout_data.dart';
 
-  @override
-  _WorkoutState createState() => _WorkoutState();
-}
-
-class _WorkoutState extends State<Workout> {
+class Workout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(Colors.blue.value),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 16,
-          ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
           child: Column(
             children: [
               Align(
@@ -26,12 +17,52 @@ class _WorkoutState extends State<Workout> {
                     onPressed: () {
                       Navigator.of(context).pop();
                     },
-                    icon: Icon(Icons.close, color: Colors.white, size: 40)),
-              )
+                    padding: EdgeInsets.all(30.0),
+                    icon: Icon(Icons.close, color: Colors.black, size: 30)),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '(Name of your workout)',
+                      style: TextStyle(fontSize: 17.0),
+                    ),
+                    Text(
+                      '(kind of workout)',
+                      style: TextStyle(fontSize: 17.0),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: workout.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      isThreeLine: true,
+                      leading: Container(
+                        width: 90.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(workout[index]['leading'][0]),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      title: Text(workout[index]['title']),
+                      subtitle: Text(
+                          '${workout[index]['subtitle']}\n${workout[index]['leading'][1]}'),
+                      trailing: workout[index]['trailing'],
+                    );
+                  },
+                ),
+              ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
