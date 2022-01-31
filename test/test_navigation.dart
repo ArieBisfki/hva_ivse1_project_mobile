acde8b450360a3d0c9686c5db6843dd5fe2a4707
KaivandenBroek <Kaivandenbroek@hotmail.com>
2022-01-31 12:17:57 +0100

    no more errors

--- test/test_navigation.dart
+++ test/test_navigation.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ivse1_gymlife/feature/calender/models/exercise.dart';
+import 'package:ivse1_gymlife/feature/calender/models/exercise_log.dart';
+import 'package:ivse1_gymlife/feature/calender/models/login_info.dart';
 import 'package:ivse1_gymlife/feature/calender/models/workoutLog.dart';
 import 'package:ivse1_gymlife/feature/calender/ui/calendar_overview.dart';
 import 'package:ivse1_gymlife/feature/calender/ui/workoutLogs_overview.dart';
+import 'package:ivse1_gymlife/feature/workout/models/exercise_data.dart';
 import 'package:ivse1_gymlife/feature/workout/ui/workout.dart';
 import 'package:mockito/mockito.dart';
 
@@ -14,7 +18,7 @@ void main() {
     await tester.pumpWidget(
       MaterialApp(
         home: Calendar(
-          loggedIn: false,
+          loginInfo: LoginInfo(loggenIn: false, ready: false),
         ),
         navigatorObservers: [mockObserver],
       ),
@@ -29,6 +33,18 @@ void main() {
         MaterialPageRoute<dynamic>(
             builder: (_) => WorkoutPage(
                   workoutLog: WorkoutLog(exerciseLogs: [], id: 0, date: ""),
+                  exerciseData: ExerciseData(
+                      id: 0,
+                      exerciseLog: ExerciseLog(
+                          exercise: Exercise(
+                              id: 0,
+                              category: 0,
+                              name: "",
+                              sets: 0,
+                              reps: 0,
+                              weight: 0,
+                              image: "",
+                              description: ""))),
                 )),
         any));
 
